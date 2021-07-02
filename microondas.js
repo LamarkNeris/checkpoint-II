a7677150b3bb1cd3e52fbae3af632754d1fc9b3d
Lamark <lamark12.neris@gmail.com>
2021-07-02 11:15:21 -0300

    checkpoint II

--- microondas.js
+++ microondas.js
@@ -1,4 +1,5 @@
 // Checkpoint: Menu Microondas
+//menu = (pipoca, macarrao, carne, feijao, brigadeiro);
 const microondas = (comida, numero) => {
 switch (comida) {
     case 'pipoca' : {
@@ -75,14 +76,40 @@ case 'brigadeiro' : {
     else if(numero > 24) {
       console.log('KABUMM')
   } 
+  break;
 }
 default : {
     console.log('comida inexistente')
 }
 }
 }    
-microondas('feijao', 50);
+microondas('feijao', 5);
+microondas('brigadeiro', 5);
+microondas('carne', 5);
+microondas('pipoca', 5);
+microondas('macarrao', 5);
+microondas('arroz', 5);
 
+microondas('feijao', 15);
+microondas('brigadeiro', 15);
+microondas('carne', 15);
+microondas('pipoca', 15);
+microondas('macarrao', 15);
+microondas('arroz', 15);
+
+microondas('feijao', 25);
+microondas('brigadeiro', 20);
+microondas('carne', 31);
+microondas('pipoca', 24);
+microondas('macarrao', 20);
+microondas('arroz', 24);
+
+microondas('feijao', 50);
+microondas('brigadeiro', 50);
+microondas('carne', 50);
+microondas('pipoca', 50);
+microondas('macarrao', 50);
+microondas('arroz', 50);
 
 
 
